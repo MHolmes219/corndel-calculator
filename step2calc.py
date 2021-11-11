bfa3cef28b9b73d0d6e77b219bb7d0cb63d06ee0
Matthew holmes <matthew.holmes@nominet.uk>
2021-11-11 14:11:03 +0000

    adjusted step2calc

--- step2calc.py
+++ step2calc.py
@@ -1,16 +1,18 @@
 def calc_file():
   with open("step_2.txt", 'r') as f:
     calc_string = f.read().splitlines()
-    print(calc_string[27])
     for string in calc_string:
       calc = string.split(' ')
-      if calc[1] == 'x':
-        print(int(calc[2]) * int(calc[3]))
-      elif calc[1] == '/':
-        print(int(calc[2]) / int(calc[3]))
-      elif calc[1] == '+':
-        print(int(calc[2]) + int(calc[3]))
-      elif calc[1] == '-':
-        print(int(calc[2]) - int(calc[3]))
+      operator = calc[1]
+      intA = int(calc[2])
+      intB = int(calc[3])
+      if operator == 'x':
+        print(intA * intB)
+      elif operator == '/':
+        print(intA / intB)
+      elif operator == '+':
+        print(intA + intB)
+      elif operator == '-':
+        print(intA - intB)
 
 calc_file()
